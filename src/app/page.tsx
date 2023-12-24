981d3abe1f4f2d841fbb0c13f317c3a2472042ed
boofi <boofiheimin@gmail.com>
2023-12-24 20:58:18 +0700

    update logo and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Grid from "@/components/Grid";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div className="p-5 font-bold text-5xl">Messages from the Heimin !</div>
+      <div className="p-5 flex justify-center items-center">
+        <Image src="/message.png" alt="me" width="350" height="175"></Image>
+      </div>
       <div className="p-5">
         <Grid></Grid>
       </div>
